c3ebd4acb447bed9238927b1fd7ac7dc02fff561
Rajish Khatiwada <rkpro98@gmail.com>
2023-09-20 17:45:03 +0545

    kg to pound done

--- app/src/main/java/com/example/unconverter/MainActivity.java
+++ app/src/main/java/com/example/unconverter/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.unconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;  //pressed alt + enter to import button
+    private TextView textView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hi click Listener worked", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = kg * 2.20462;
+                textView.setText("The corresponding value in pound is " + pound);
+            }
+        });
     }
 }
